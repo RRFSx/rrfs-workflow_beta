1a341d491678e2bab3cefa1c57f78eedc4f9197f
Guoqing Ge <guoqing.ge@noaa.gov>
2024-07-20 21:56:49 -0600

    changes to facilitate a realtime run on jet for hrrrv5 (#31)

--- scripts/exrrfs_upp.sh
+++ scripts/exrrfs_upp.sh
@@ -33,7 +33,7 @@ EOF
 ulimit -s unlimited
 ulimit -v unlimited
 ulimit -a
-### temporarily solution since upp uses modules files different from other components
+### temporarily solution as UPP uses modules different from other components
 set +x # supress messy output in the module load process
 module purge
 module use /mnt/lfs5/BMC/wrfruc/HRRRv5/UPP/modulefiles
@@ -42,18 +42,42 @@ module use /lfs5/BMC/nrtrr/FIX_RRFS2/modulefiles
 module load prod_util/2.1.1
 module list
 set -x  
-### temporarily solution since mpassit uses different modules files that other components
+### temporarily solution as UPP uses modules different from other components
 source prep_step
 srun /lfs5/BMC/nrtrr/FIX_RRFS2/exec/upp.x #gge.debug temp solution
 # check the status copy output to COMOUT
-if [[ -s "./geguoqing" ]]; then
-  ${cpreq} ${DATA}/${FHR}/mpassit.${timestr}.nc ${COMOUT}/${task_id}/
-else
-  echo "FATAL ERROR: failed to genereate mpassit.${timestr}.nc"
+wrfprs="WRFPRS${fhr}.tm00"
+wrfnat="WRFNAT${fhr}.tm00"
+wrftwo="WRFTWO${fhr}.tm00"
+if [[ ! -s "./${wrfprs}" ]]; then
+  echo "FATAL ERROR: failed to genereate WRF grib2 files"
   export err=99
   err_exit
 fi
 
-### generate other grib2 files
+### generate final grib2 products
 WGRIB2=/apps/wgrib2/2.0.8/intel/18.0.5.274/bin/wgrib2
-PY4GRIB=/contrib/miniconda3/4.5.12/envs/avid_verify/bin/python
+HRRR_DIR="/public/data/grib/hrrr_wrfprs/7/0/83/0_1905141_30" #COMINhrrr
+YYJJJHH=$(date -d "${CDATE:0:8} ${CDATE:8:2}:00" +%y%j%H)
+# copy selected fields from netcdf to grib2, using HRRR grib2 as a template
+${WGRIB2} ${HRRR_DIR}/${YYJJJHH}0000${fhr} -match ":TSOIL:" -set_grib_type simple -grib_out TSOIL_template.grib2
+${WGRIB2} ${HRRR_DIR}/${YYJJJHH}0000${fhr} -match ":SOILW:" -set_grib_type simple -grib_out SOILW_template.grib2
+${WGRIB2} ${HRRR_DIR}/${YYJJJHH}0000${fhr} -match ":VGTYP:" -set_grib_type simple -grib_out VGTYP.grib2
+PY=/contrib/miniconda3/4.5.12/envs/avid_verify/bin/python
+SCRIPTS="/lfs5/BMC/nrtrr/FIX_RRFS2/exec"
+${PY} ${SCRIPTS}/netcdf_to_grib.py mpassit.${timestr}.nc TSLB TSOIL_template.grib2 TSOIL.grib2
+${PY} ${SCRIPTS}/netcdf_to_grib.py mpassit.${timestr}.nc SMOIS SOILW_template.grib2 SOILW.grib2
+cat ${wrftwo} TSOIL.grib2 SOILW.grib2 VGTYP.grib2 > ${wrftwo}.tmp
+mv ${wrftwo}.tmp ${wrftwo}
+
+# Append the 2D fields onto the 3D files
+cat ${wrfprs} ${wrftwo} > ${wrfprs}.tmp
+mv ${wrfprs}.tmp ${wrfprs}
+cat ${wrfnat} ${wrftwo} > ${wrfnat}.two
+mv ${wrfnat}.two ${wrfnat}
+
+# copy products to COMOUT # file name does not comply with NCO standards #gge.debug tmp
+${cpreq} ${wrfprs} ${COMOUT}/${RUN}_prs_${CDATE}_f${fhr}.grib2
+${cpreq} ${wrfnat} ${COMOUT}/${RUN}_nat_${CDATE}_f${fhr}.grib2
+${cpreq} ${wrftwo} ${COMOUT}/${RUN}_two_${CDATE}_f${fhr}.grib2
+${cpreq} ${wrfprs} ${COMOUT}/${YYJJJHH}0000${fhr}
