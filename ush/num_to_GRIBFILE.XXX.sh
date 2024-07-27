fc32433f0cc79278ef7112143476457c9faee0da
guoqing.ge <Guoqing.Ge@noaa.gov>
2024-07-26 20:59:46 -0600

    change PS4 in one ush script

--- ush/num_to_GRIBFILE.XXX.sh
+++ ush/num_to_GRIBFILE.XXX.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # convert a number to the ungrib file naming convention: GRIBFILE.AAA
-export PS4="+ $(basename ${BASH_SOURCE})[${LINENO}]:"
+declare -rx PS4='+ $(basename ${BASH_SOURCE[0]:-${FUNCNAME[0]:-"Unknown"}})[${LINENO}]${id}: '
 num=$((10#$1))
 [[ "${num}" == "0" ]] && exit
 num=$((10#$num-1))
